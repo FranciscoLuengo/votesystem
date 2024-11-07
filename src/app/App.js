d97bb97f4c1b3381920f900c49239b40ed1db9ae
XIAOSENNIN <xiaosennin@gmail.com>
2024-11-07 02:08:58 -0300

    se agrego toast msg, fix del nuevo contrato

--- src/app/App.js
+++ src/app/App.js
@@ -7,8 +7,10 @@ import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import { Alert, AlertDescription, AlertTitle } from "@/components/ui/alert";
 import { Wallet, Plus, Trash2, Vote, XCircle } from "lucide-react";
 import { contractABI, contractAddress } from "./datos";
+import { useToast } from "@/hooks/use-toast";
 
 export default function App() {
+    const { toast } = useToast();
     const [address, setAddress] = useState("");
     const [isAdmin, setIsAdmin] = useState(false);
     const [proposals, setProposals] = useState([]);
@@ -16,6 +18,7 @@ export default function App() {
     const [loading, setLoading] = useState(false);
     const [errorMessage, setErrorMessage] = useState("");
     const [notification, setNotification] = useState("");
+    const [instalar, setInstalar] = useState(false);
 
     const connectWallet = async () => {
         setLoading(true);
@@ -44,13 +47,23 @@ export default function App() {
                     userAddress.toLowerCase() === chairPerson.toLowerCase()
                 );
 
-                setNotification("Conectado con éxito");
+                toast({
+                    variant: "success",
+                    title: "Success",
+                    description: "Conectado con éxito",
+                });
+                setInstalar(false);
             } else {
                 setErrorMessage("MetaMask no está instalado");
+                setInstalar(true);
             }
         } catch (error) {
             console.error("Error al conectar MetaMask:", error);
-            setErrorMessage("Error al conectar MetaMask");
+            if (error.code === -32002) {
+                setErrorMessage("Inicie sesion en MetaMask para continuar");
+            } else {
+                setErrorMessage("Error al conectar MetaMask");
+            }
         } finally {
             setLoading(false);
         }
@@ -63,6 +76,11 @@ export default function App() {
         } catch (error) {
             console.error("Error al obtener las propuestas:", error);
             setErrorMessage("Error al obtener las propuestas");
+            toast({
+                variant: "destructive",
+                title: "Error",
+                description: "Error al obtener las propuestas",
+            });
         }
     };
 
@@ -80,10 +98,18 @@ export default function App() {
             await fetchProposals(contract);
             setErrorMessage("");
             setNotification("Propuesta agregada con éxito.");
+            toast({
+                variant: "success",
+                title: "Success",
+                description: "Propuesta agregada con éxito.",
+            });
         } catch (error) {
             console.error("Error al agregar propuesta:", error);
-            setErrorMessage("Error al agregar propuesta.");
-            setNotification("");
+            toast({
+                variant: "destructive",
+                title: "Error",
+                description: "Error al agregar propuesta.",
+            });
         } finally {
             setLoading(false);
         }
@@ -101,10 +127,20 @@ export default function App() {
             await tx.wait();
             await fetchProposals(contract);
             setErrorMessage("");
-            setNotification("Propuesta eliminada con éxito.");
+            //setNotification("Propuesta eliminada con éxito.");
+            toast({
+                variant: "success",
+                title: "Success",
+                description: "Propuesta eliminada con éxito.",
+            });
         } catch (error) {
             console.error("Error al eliminar propuesta:", error);
-            setErrorMessage("Error al eliminar propuesta.");
+            //setErrorMessage("Error al eliminar propuesta.");
+            toast({
+                variant: "destructive",
+                title: "Error",
+                description: "Error al eliminar propuesta.",
+            });
             setNotification("");
         } finally {
             setLoading(false);
@@ -124,12 +160,26 @@ export default function App() {
             await fetchProposals(contract);
             setErrorMessage("");
             setNotification("Voto emitido con éxito.");
+            toast({
+                variant: "success",
+                title: "Success",
+                description: "Voto emitido con éxito.",
+            });
         } catch (error) {
             console.error("Error al votar:", error);
-            setErrorMessage(
-                "Error al votar. Ya has votado / Error en la transacción"
-            );
-            setNotification("");
+            if (error.code === 4001 || error.code === "ACTION_REJECTED") {
+                toast({
+                    variant: "destructive",
+                    title: "Error",
+                    description: "Error en la transacción.",
+                });
+            } else {
+                toast({
+                    variant: "destructive",
+                    title: "Error",
+                    description: "Ya has votado.",
+                });
+            }
         } finally {
             setLoading(false);
         }
@@ -146,12 +196,18 @@ export default function App() {
             const tx = await contract.removeVote();
             await tx.wait();
             await fetchProposals(contract);
-            setErrorMessage("");
-            setNotification("Voto eliminado con éxito.");
+            toast({
+                variant: "success",
+                title: "Success",
+                description: "Voto eliminado con éxito.",
+            });
         } catch (error) {
             console.error("Error al eliminar voto:", error);
-            setErrorMessage("Error al eliminar voto.");
-            setNotification("");
+            toast({
+                variant: "destructive",
+                title: "Error",
+                description: "Error al eliminar voto.",
+            });
         } finally {
             setLoading(false);
         }
@@ -335,6 +391,115 @@ export default function App() {
                                 </div>
                             )}
                         </>
+                    )}
+                    {instalar && (
+                        <div className="mt-10">
+                            <h1>Instrucciones para instalar MetaMask</h1>
+                            <ul className="list-disc pl-5">
+                                {/* Paso 1 */}
+                                <li>
+                                    <strong>Descargar MetaMask:</strong>
+                                    <ul className="list-disc pl-5">
+                                        <li>
+                                            Visita la{" "}
+                                            <a
+                                                href="https://metamask.io/download.html"
+                                                target="_blank"
+                                                rel="noopener noreferrer"
+                                                className="text-blue-600 underline"
+                                            >
+                                                página oficial de MetaMask
+                                            </a>
+                                            .
+                                        </li>
+                                        <li>
+                                            Selecciona tu navegador favorito
+                                            (Chrome, Firefox, Edge, etc) o
+                                            instala la aplicación para móvil.
+                                        </li>
+                                        <li>
+                                            Haz clic en “Agregar a Chrome” o el
+                                            navegador que uses para instalar la
+                                            extensión.
+                                        </li>
+                                    </ul>
+                                </li>
+
+                                {/* Paso 2 */}
+                                <li>
+                                    <strong>Configurar MetaMask:</strong>
+                                    <ul className="list-disc pl-5">
+                                        <li>
+                                            Abre la extensión después de
+                                            instalarla.
+                                        </li>
+                                        <li>
+                                            Crea una nueva billetera o importa
+                                            una existente.
+                                        </li>
+                                        <li>
+                                            Guarda tu frase de recuperación en
+                                            un lugar seguro; no la compartas con
+                                            nadie.
+                                        </li>
+                                    </ul>
+                                </li>
+
+                                {/* Paso 3 */}
+                                <li>
+                                    <strong>Activar redes de prueba:</strong>
+                                    <ul className="list-disc pl-5">
+                                        <li>
+                                            Abre MetaMask y haz clic en el icono
+                                            de tu cuenta en la esquina superior
+                                            derecha.
+                                        </li>
+                                        <li>
+                                            Selecciona “Configuración“ &gt;
+                                            “Redes“ &gt; “Mostrar redes de
+                                            prueba“. Actívalo para ver la red de
+                                            prueba que necesitamos, en este caso
+                                            <strong> Sepolia</strong>
+                                        </li>
+                                    </ul>
+                                </li>
+
+                                {/* Paso 4 */}
+                                <li>
+                                    <strong>
+                                        Obtener fondos de prueba (faucet):
+                                    </strong>
+                                    <ul className="list-disc pl-5">
+                                        <li>
+                                            Elige la red de prueba{" "}
+                                            <strong>Sepolia</strong> en MetaMask
+                                        </li>
+                                        <li>
+                                            Visita{" "}
+                                            <a
+                                                href="https://cloud.google.com/application/web3/faucet"
+                                                target="_blank"
+                                                rel="noopener noreferrer"
+                                                className="text-blue-600 underline"
+                                            >
+                                                Google Faucet
+                                            </a>
+                                        </li>
+                                        <li>
+                                            Ingresa la red de prueba{" "}
+                                            <strong>Sepolia</strong> y dirección
+                                            de tu billetera (disponible en
+                                            MetaMask bajo tu nombre de cuenta) y
+                                            solicita fondos de prueba.
+                                        </li>
+                                        <li>
+                                            Espera unos minutos para que los
+                                            fondos aparezcan en tu cuenta.
+                                        </li>
+                                    </ul>
+                                </li>
+                            </ul>
+                        </div>
                     )}
 
                     {errorMessage && (
